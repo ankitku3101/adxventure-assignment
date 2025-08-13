8e107938f9fa803e605591672f2b5e4a9d6934a4
ankitku3101 <ankit.ku3101@gmail.com>
2025-08-13 15:08:37 +0530

    feat(web): add components and section boilerplates

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Landing from "@/sections/Landing";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      Adxventure Assignment | Start Time: 3:00 PM IST 13-08-2025
+      <Landing />
     </>
   );
 }
---
+++ sections/Landing.tsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+        <Header />
+        <Hero />
+        <Footer />
+    </div>
+  )
+}
+
+export default Landing
